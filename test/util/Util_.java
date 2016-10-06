9b3d5555653dcd73ed976bcdf2651f9e88337900
Jesé Romero Arbelo <jese540728@gmail.com>
2016-10-06 15:35:10 +0200

    Updated gitignore and Util_ delay was incremented.

--- test/util/Util_.java
+++ test/util/Util_.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 public class Util_ {
 
-    @Test (timeout=210)
+    @Test (timeout=300)
     public void simulates_thread_sleep() throws Exception {
         Util.sleep(200);
     }
